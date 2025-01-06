da8b5bfeb28653671499a8fea58000683c3c5931
wowinter13 <vla-dy@yandex.ru>
2025-01-06 23:42:49 +0100

    chore(release): bump version to 1.0.0

--- Formula/pkcs12cracker.rb
+++ Formula/pkcs12cracker.rb
@@ -4,9 +4,9 @@
 class Pkcs12cracker < Formula
   desc "High-performance, multi-threaded PKCS#12 password cracker written in Rust. Supports dictionary, pattern-based, and brute force attacks with focus on performance and reliability."
   homepage "https://github.com/wowinter13/pkcs12cracker"
-  url "https://github.com/wowinter13/pkcs12cracker/releases/download/0.1.0/pkcs12cracker-mac.tar.gz"
-  sha256 "20f7e833ea2f7cdf99c5fe9a36e2c65873ac6002a2aa45b916d606d6e85a9356"
-  version "0.1.0"
+  url "https://github.com/wowinter13/pkcs12cracker/releases/download/1.0.0/pkcs12cracker-mac.tar.gz"
+  sha256 "41eb1d7309ec2162bdac2f1e14af75eed2f8f010fbfe052cf6825f559464297f"
+  version "1.0.0"
 
   def install
     bin.install "pkcs12cracker"
